66c0099236e96821c7bb97b323a947b7f984bb04
philpill <github@philpill.net>
2025-03-30 21:33:07 +0100

    css

--- index.js
+++ index.js
@@ -1,45 +1,18 @@
 const express = require('express');
-const Transform = require("stream").Transform;
-const fs = require('fs');
+
 
 const path = require('path');
 const listData = require('./data.json');
+const venueData = require('./venue.json');
 const app = express()
 const port = 3000
 
-const populateData = new Transform();
+app.set('view engine', 'pug')
 
-populateData._transform = function(data, encoding, done) {
+app.use('/static', express.static(__dirname + '/static'))
 
-    console.log('listData', listData);
-
-    const arr = [];
-
-    for (let i = 0; i < listData.length; i++) {
-        arr.push(`<li>${listData[i].Location}</li>`);
-    }
-
-    const result = arr.join('');
-
-    const str = data.toString();
-    const thing = str.replace('{{ data }}', result);
-    this.push(thing);
-    done();
-}
-
-app.use(express.static(__dirname + '/public'));
-
-// https://stackoverflow.com/a/66055532/3983822
 app.get('/', (req, res) => {
-
-    const file = './index.html';
-
-    res.write('<!-- Begin stream -->\n');
-    let stream = fs.createReadStream(file, { encoding: 'utf8' });
-    stream.pipe(populateData)
-    .on('end', () => {
-        res.write('\n<!-- End stream -->')
-    }).pipe(res);
+    res.render('index', { foodData: venueData.filter(item => item.category === 'food') });
 })
 
 app.listen(port, () => {
