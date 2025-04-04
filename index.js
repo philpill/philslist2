8e611d2b95be824cce05ac303df6ff72b51bb6fb
philpill <github@philpill.net>
2025-04-04 20:47:14 +0100

    google spreadsheet

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet')
 const express = require('express');
+console.log(require('dotenv').config());
 
-
-const path = require('path');
-const listData = require('./data.json');
 const venueData = require('./venue.json');
 const app = express()
 const port = 3000
@@ -12,6 +11,13 @@ app.set('view engine', 'pug')
 app.use('/static', express.static(__dirname + '/static'))
 
 app.get('/', (req, res) => {
+
+    (async function () {
+        const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID, { apiKey: process.env.GOOGLE_API_KEY });
+        await doc.loadInfo();
+        console.log(doc.title);
+    })();
+
     res.render('index', { foodData: venueData.filter(item => item.category === 'food') });
 })
 
