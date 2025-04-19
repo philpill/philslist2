307115ddf42ae41321f938f25eafc88cb88fa4ae
philpill <github@philpill.net>
2025-04-19 20:00:59 +0100

    rows

--- index.js
+++ index.js
@@ -12,18 +12,10 @@ app.use('/static', express.static(__dirname + '/static'))
 
 app.get('/', (req, res) => {
 
-    (async function () {
-        const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID, { apiKey: process.env.GOOGLE_API_KEY });
-        await doc.loadInfo();
-        console.log(doc.title);
-    })();
-
     res.render('index', { foodData: venueData.filter(item => item.category === 'food') });
 })
 
 app.get('/update', (req, res) => {
-    console.log(req);
-    console.log(req.query);
 
     if (req.query && req.query.key && req.query.key === process.env.TRIGGER_KEY) {
 
@@ -34,8 +26,15 @@ app.get('/update', (req, res) => {
     
             const sheet = doc.sheetsByIndex[1];
             console.log(sheet.title);
-        })();
 
+            const rows = await sheet.getRows();
+
+            rows.forEach(row => {
+                let name = row.get('name');
+                console.log(name);
+            });
+
+        })();
     }
 
     res.send('ok')
